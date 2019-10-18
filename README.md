2a25af5f2f9eaeddfe5517d9fc1bcd5024083b64
Kimberlyn Creary <kimocreary@gmail.com>
2019-10-18 12:28:04 -0400

    movie, spotify, concert added

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # -liri-node-app
+
+LIRI will be a command line node app that takes in parameters and gives you back data.
+
+LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+
+LIRI will send requests using the axios package to the Bands in Town, Spotify and OMDB APIs
+
+
+
+
+Clearly state the problem the app is trying to solve (i.e. what is it doing and why)
+Give a high-level overview of how the app is organized
+Give start-to-finish instructions on how to run the app
+Include screenshots, gifs or videos of the app functioning
+Contain a link to a deployed version of the app
+Clearly list the technologies used in the app
+State your role in the app development
+
+
--- liri.js
+++ liri.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
+
 var Spotify = require("node-spotify-api"); 
 var keys = require("./keys"); 
 var axios = require("axios"); 
 var moment = require("moment");
 var spotify = new Spotify(keys.spotify);
+var fs = require("fs");
 var command = process.argv[2];
 var searchString = process.argv.slice(3).join(" ") 
 switch(command) {
@@ -31,12 +33,17 @@ function songName() {
          query: searchSong
     },function(error,data){
         if (error) throw error;
-        console.log(data, "response from spotify api")
+        console.log(data, "response from spotify api");
+        console.log("album url:",data.tracks.items[0].album.href);
+        console.log(data.tracks.items[0].artists[0].name);
+        console.log("album name:",data.tracks.items[0].album.name);
+        console.log(data.tracks.items[0].preview_url || data.tracks.items[0].href);
+        console.log(data.tracks.items[0].name);
     }
     );
 }
 function movieTitle() {
-    var searchMovie = searchString || "The Sign by Ace of Base"
+    var searchMovie = searchString || "Armageddon"
     var queryUrl = "http://www.omdbapi.com/?t=" + searchMovie + "&y=&plot=full&tomatoes=true&apikey=trilogy"; 
     axios.get(queryUrl).then(function(response){
         console.log(response.data)
@@ -47,5 +54,17 @@ function artistShowDetails() {
     var queryUrl = "https://rest.bandsintown.com/artists/" + searchConcert + "/events?app_id=codingbootcamp";
     axios.get(queryUrl).then(function(response){
         console.log(response.data)
+        // response.data and navigate items
+    })
+}
+function executeCommand() {
+    fs.readFile("random.txt","utf8",function(error,data){
+        if (error) throw error;
+        console.log(data);
+        var dataarray = data.split(",");
+        if (dataarray[0] === "spotify-this-song") {
+            searchString = dataarray[1]
+            songName();
+        }
     })
 }
